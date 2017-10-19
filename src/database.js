9ffe84eb651742eb04403beba89d9a4376f730ec
Andres Arias <andres.arias12@gmail.com>
2017-10-19 11:10:05 -0600

    Database file

---
+++ src/database.js
@@ -0,0 +1,8 @@
+var database = {};
+
+database.main = "mongodb://192.168.100.6:5020/Tienda";
+// TODO: Replica set
+//database.backupA = "mongodb://192.168.100.6:8050/Alajuela";
+//database.backupB = "mongodb://192.168.100.15:8052/Heredia";
+
+module.exports = database;
