58a94d8580a610392b313983a8be42b191f82c70
Andres Arias <andres.arias12@gmail.com>
2017-10-21 19:06:40 -0600

    Adaptado login a la nueva base, arreglos en db.close

--- src/client.js
+++ src/client.js
@@ -4,14 +4,6 @@ var MongoClient = require('mongodb').MongoClient; // TODO: NPM install
 var database = require('./database.js');
 var bodyParser = require("body-parser"); // TODO: NPM install
 
-// ======== DATABASE CONNECTIONS ========
-var mainDB;
-
-MongoClient.connect(database.main, function (err, db) {
-    if (err) { return console.dir(err) }
-    mainDB = db;
-});
-
 // ======== BODY PARSER INIT ========
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
@@ -22,30 +14,38 @@ router.use(bodyParser.json());
 router.post('/login', function (req, res)
 {
     var loginInfo = req.body; // Gets user data from request's body.
-    mainDB.collection("cliente").find( // Find user with matching data.
+    MongoClient.connect(database.main, function(err, db) {
+        var collection = db.collection('Clients');
+        collection.find( // Find user with matching data.
         {
         "correo": loginInfo.correo,
-        "clave": loginInfo.clave}).toArray(
+        "password": loginInfo.password}).toArray(
             function(error, result)
             {
                 if (result.length == 0) // No user found with given details.
                 {
+                    db.close();
                     res.json({"result" : 0});
                 } else // User found
                 {
+                    db.close();
                     // Returns a success signal (1) and its id.
                     res.json({"result" : 1, "email" : result[0].correo});
                 }
             }
         );
+    });
 });
 
 // Order insertion function: 
 router.post('/newOrder', function(req, res) {
     var newOrder = req.body; // Gets data from request's body.
-    var collection = mainDB.collection('ordenes');
-    collection.insert(newOrder); // Simple insert.
+    MongoClient.connect(database.main, function(err, db) {
+        var collection = db.collection('Orders');
+        collection.insert(newOrder); // Simple insert.
+        db.close();
     res.json({"Success" : 1}); // Returns success signal.
+    });
 });
 
 module.exports = router;
