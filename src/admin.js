100cacdef40a87bd51a74b543d3e977cd51c08d2
Andres Arias <andres.arias12@gmail.com>
2017-10-21 23:33:38 -0600

    Top 3 (Me costo un huevo, fuck)

--- src/admin.js
+++ src/admin.js
@@ -8,11 +8,11 @@ var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-// ======== MAIN FUNCTIONS ========
-function retrieveOrders(user, callback) {
+// ======== DB ACCESS FUNCTIONS ========
+function retrieveOrders(user, callback) { // Finds orders by users.
     MongoClient.connect(database.main, function (err, db) {
         var collection = db.collection('Orders');
-        collection.find({"cliente": user}).toArray(function (error, result) {
+        collection.find({ "cliente": user }).toArray(function (error, result) {
             if (error) throw error;
             db.close();
             callback(result);
@@ -20,31 +20,133 @@ function retrieveOrders(user, callback) {
     });
 }
 
+function retrieveAllOrders(callback) { // Finds all orders.
+    MongoClient.connect(database.main, function (err, db) {
+        var collection = db.collection('Orders');
+        collection.find({}).toArray(function (error, result) {
+            if (error) throw error;
+            db.close();
+            callback(result);
+        });
+    });
+}
+
+// ======== BACKUP FUNCTIONS ========
 function amountProducts(orders) {
     var amounts = [];
-    for (var i = 0 ; i < orders.length ; i++) {
+    for (var i = 0; i < orders.length; i++) {
         var articles = orders[i].articulos;
         var total = 0;
-        for (var j = 0 ; j < articles.length ; j++) {
+        for (var j = 0; j < articles.length; j++) {
             total += Number(articles[j].cantidad);
         }
         amounts.push(total);
     }
-    
+
     return amounts;
 }
 
-// ======== ROUTES ========
+function extractProducts(orders) {
+    var products = [];
+    for (var i = 0; i < orders.length; i++) {
+        var articles = orders[i].articulos;
+        for (var j = 0; j < articles.length; j++) {
+            products.push(articles[j]);
+        }
+    }
 
+    return products;
+}
+
+function removeProduct(product, list) {
+    var result = list.filter(function(a) {
+        return a !== product
+    });
+
+    return result;
+}
+
+function countMentions(product, list) {
+    var result = 0;
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] == product) result++
+    }
+
+    return result;
+}
+
+function countProducts(products) {
+    var productCount = [];
+    var result = [];
+    for (var j = 0; j < products.length; j++) {
+        productCount.push(products[j].articulo);
+    }
+
+    while (productCount.length != 0) {
+        var current = productCount[0];
+        var mentions = {
+            "product": current,
+            "times": countMentions(current, productCount)
+        };
+        result.push(mentions);
+        productCount = removeProduct(current, productCount);
+    } 
+
+    return result;
+}
+
+function findHigher(products) {
+    var mayor = products[0];
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].times > mayor.times) {
+            mayor = products[i];
+        }
+    }
+
+    return mayor;
+}
+
+function getTop(orders) {
+    var products = extractProducts(orders);
+    console.log(products);
+    var counts = countProducts(products);
+    var count = 0;
+    var top = [];
+    while (count < 3) {
+        var higher = findHigher(counts);
+        top.push(higher.product);
+        //top.push(higher);
+        var index = counts.indexOf(higher);
+        counts.splice(index, 1);
+        count++;
+    }
+
+    return top;
+}
+
+// ======== ROUTES ========
 router.get('/range/:id', function (req, res) {
     var user = req.params.id;
-    retrieveOrders(user, function(data) {
+    retrieveOrders(user, function (data) {
         var amounts = amountProducts(data);
         var ranges = {
             "max": Math.max.apply(Math, amounts),
             "min": Math.min.apply(Math, amounts)
         };
         res.json(ranges);
+    });
+});
+
+router.get('/top', function(req, res) {
+    retrieveAllOrders(function(data) {
+        var top = getTop(data);
+        var top3 = {
+            "first": top[0],
+            "second": top[1],
+            "third": top[2]
+        };
+
+        res.json(top3);
     });
 });
 
